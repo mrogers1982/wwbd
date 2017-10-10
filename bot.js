bf8f64f27f93b97340096710ccd89bdf9a6ff502
Michael J. Rogers <mrogers@lmtc-mroge.jomax.paholdings.com>
2017-10-09 19:32:38 -0700

    changed response

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = 'sheeeeeet';
 
   options = {
     hostname: 'api.groupme.com',
